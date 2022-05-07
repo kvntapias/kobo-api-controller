ad18e6d19b4cfb6fd40315154e360d2bc340be9b
Kevin Tapias <kltapias@cispcolombia.org>
2022-05-07 11:39:27 -0500

    index para familiar

---
+++ resources/views/build_pdf/templates/familiar_2022/index.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="{{asset('lib/bootstrap/css/bootstrap.min.css')}}" type="text/css">
+        {{-- <link rel="stylesheet" href="{{asset('lib/bootstrap3/bootstrap3.min.css')}}" type="text/css"> --}}
+        {{-- <link rel="stylesheet" href="{{asset('css/build_pdf/familiar_2022.css')}}" type="text/css"> --}}
+        <style>
+            @page { margin: 40px 40px 40px; }
+        </style>
+    </head>
+    <body>
+        <main>
+            {{-- @include('build_pdf.templates.source.pages.page_1') --}}
+        </main>
+    </body>
+</html>
