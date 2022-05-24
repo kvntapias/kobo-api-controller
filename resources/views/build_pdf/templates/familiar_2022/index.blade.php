80aa4122c68d9540b05e6aa788e69adf697dcaa8
Kevin Tapias <kltapias@cispcolombia.org>
2022-05-24 10:53:26 -0500

    inicializar recursos para esq. familiar

--- resources/views/build_pdf/templates/familiar_2022/index.blade.php
+++ resources/views/build_pdf/templates/familiar_2022/index.blade.php
@@ -2,15 +2,14 @@
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="{{asset('lib/bootstrap/css/bootstrap.min.css')}}" type="text/css">
-        {{-- <link rel="stylesheet" href="{{asset('lib/bootstrap3/bootstrap3.min.css')}}" type="text/css"> --}}
-        {{-- <link rel="stylesheet" href="{{asset('css/build_pdf/familiar_2022.css')}}" type="text/css"> --}}
+        <link rel="stylesheet" href="{{asset('css/build_pdf/familiar_2022.css')}}" type="text/css">
         <style>
             @page { margin: 40px 40px 40px; }
         </style>
     </head>
     <body>
         <main>
-            {{-- @include('build_pdf.templates.source.pages.page_1') --}}
+            @include('build_pdf.templates.familiar_2022.pages.page_1')
         </main>
     </body>
 </html>
---
+++ resources/views/build_pdf/templates/familiar_2022/pages/page_1.blade.php
@@ -0,0 +1,5 @@
+<div class="seccion_head mt-1">
+    <h4 class="text-center">
+      ESQUEMAS ESPECIALES DE ACOMPAÑAMIENTO FAMILIAR
+    </h4>
+</div>
