27294276bd3070a43658ef32dca65ec3e37aa378
Kevin Tapias <kltapias@cispcolombia.org>
2022-05-24 10:14:57 -0500

    intento de fonts - pendiente x integrar completamente

--- resources/views/build_pdf/templates/eeac_2022/index.blade.php
+++ resources/views/build_pdf/templates/eeac_2022/index.blade.php
@@ -2,10 +2,17 @@
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="{{asset('lib/bootstrap/css/bootstrap.min.css')}}" type="text/css">
-        <link rel="stylesheet" href="{{asset('css/build_pdf/eeac_2022.css')}}" type="text/css">
+        <link rel="stylesheet" href="{{asset('css/build_pdf/eeac_2022.css?v=').round(microtime(true))}}" type="text/css">
         <script src="{{ asset('lib/jq.min.js')}}"></script>
         <style>
             @page { margin: 40px 40px 40px; }
+
+            /* @font-face {
+                font-family: 'Open Sans';
+                font-style: normal;
+                font-weight: bold;
+                src: {{ asset('css/fonts/prima-sans-bold.ttf') }}
+            } */
         </style>
     </head>
     <body>
