baa978e0cfb32d006d4730bc11d8d9f75d1470bf
Kevin Tapias <kltapias@cispcolombia.org>
2022-06-08 08:56:57 -0500

    evaluar si tiene grupo de respuesta

--- resources/views/build_pdf/templates/familiar_2022/pages/grupo_fam.blade.php
+++ resources/views/build_pdf/templates/familiar_2022/pages/grupo_fam.blade.php
@@ -1,38 +1,40 @@
-<!--INTEGRANTES HOGAR-->
-<div class="contenedor_ppal">
-    <table style="width: 100%; page-break-inside: avoid;" class="table">
-        <thead>
-            <tr>
-              <td colspan="2">
-                <div class="seccion_head">
-                  <h4 class="text-center">
-                    INTEGRANTES DEL HOGAR
-                  </h4>
-                </div>
-              </td>
-            </tr>
-          </thead>
-        <tbody>
-            @foreach ($build_pdf->imprimir_grupo_respuestas('group_wu4ss89/grupo_familiar_01') as $grup_rpta)
-                @switch($grup_rpta['type'])
-                    @case('text')
-                    @case('select_one')
-                    @case('select_multiple')
-                    @case('date')
-                        <tr>
-                            <td>
-                                {{ $grup_rpta['pregunta'] }} :
-                            </td>
-                            <td>
-                                {{ $grup_rpta['respuesta'] }}
-                            </td>
-                        </tr>
-                    @break             
-                @endswitch
-                @if ($grup_rpta['key'] == "Tipo_de_documento")
-                    <hr>
-                @endif
-            @endforeach
-        </tbody>
-    </table>
-</div>
+@if ($build_pdf->imprimir_grupo_respuestas('group_wu4ss89/grupo_familiar_01'))
+    <!--INTEGRANTES HOGAR-->
+    <div class="contenedor_ppal">
+        <table style="width: 100%; page-break-inside: avoid;" class="table">
+            <thead>
+                <tr>
+                <td colspan="2">
+                    <div class="seccion_head">
+                    <h4 class="text-center">
+                        INTEGRANTES DEL HOGAR
+                    </h4>
+                    </div>
+                </td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($build_pdf->imprimir_grupo_respuestas('group_wu4ss89/grupo_familiar_01') as $grup_rpta)
+                    @switch($grup_rpta['type'])
+                        @case('text')
+                        @case('select_one')
+                        @case('select_multiple')
+                        @case('date')
+                            <tr>
+                                <td>
+                                    {{ $grup_rpta['pregunta'] }} :
+                                </td>
+                                <td>
+                                    {{ $grup_rpta['respuesta'] }}
+                                </td>
+                            </tr>
+                        @break             
+                    @endswitch
+                    @if ($grup_rpta['key'] == "Tipo_de_documento")
+                        <hr>
+                    @endif
+                @endforeach
+            </tbody>
+        </table>
+    </div> 
+@endif
